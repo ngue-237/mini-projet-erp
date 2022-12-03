c5e8d05af962dbcee55290974ff88207a75f23f4
ngueemmanuel.prof@gmail.com <ngueemmanuel.prof@gmail.com>
2022-12-03 13:25:49 +0100

    all crud operation work

--- location-webservice/src/main/java/com/miniprojet/locationwebservice/repository/ClientRepository.java
+++ location-webservice/src/main/java/com/miniprojet/locationwebservice/repository/ClientRepository.java
@@ -4,4 +4,5 @@ import com.miniprojet.locationwebservice.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository  extends JpaRepository<Client,Long> {
+    public Client findByEmailIgnoreCase(String email);
 }
--- location-webservice/src/main/java/com/miniprojet/locationwebservice/service/client/ClientServiceImplement.java
+++ location-webservice/src/main/java/com/miniprojet/locationwebservice/service/client/ClientServiceImplement.java
@@ -5,6 +5,9 @@ import com.miniprojet.locationwebservice.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
 @Service
 public class ClientServiceImplement implements ClientServiceInterface {
     @Autowired
@@ -17,5 +20,40 @@ public class ClientServiceImplement implements ClientServiceInterface {
     @Override
     public Client saveClient(Client clientSaved) {
         return clientRepo.save(clientSaved);
+    }
+
+    @Override
+    public Client getClientByEmail(String email) {
+        return clientRepo.findByEmailIgnoreCase(email);
+    }
+
+    @Override
+    public Client updateClientByEmail(String email, Client clientModified) {
+        System.out.println(clientModified);
+        Client clientDb = clientRepo.findByEmailIgnoreCase(email);
+        if(Objects.nonNull(clientModified.getNom()) && !"".equalsIgnoreCase(clientModified.getNom())){
+            clientDb.setNom(clientModified.getNom());
+        }
+        if(Objects.nonNull(clientModified.getPrenom()) && !"".equalsIgnoreCase(clientModified.getPrenom())){
+            clientDb.setPrenom(clientModified.getPrenom());
+        }
+        if(Objects.nonNull(clientModified.getCni()) && !"".equalsIgnoreCase(clientModified.getCni())){
+            clientDb.setCni(clientModified.getCni());
+        }
+        if(Objects.nonNull(clientModified.getEmail()) && !"".equalsIgnoreCase(clientModified.getEmail())){
+            clientDb.setEmail(clientModified.getEmail());
+        }
+
+        return clientRepo.save(clientDb);
+    }
+
+    @Override
+    public void deleteClientById(Long clientId) {
+        clientRepo.deleteById(clientId);
+    }
+
+    @Override
+    public List<Client> getAllClient() {
+        return clientRepo.findAll();
     }
 }
