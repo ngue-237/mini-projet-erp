1364e2e20c6e35210c6259c72fe0e09b7fc6766c
Emmanuel Benjamin <ngueemmanuel.prof@gmail.com>
2022-12-09 15:01:08 +0100

    user service added

--- gestion-user/src/main/java/com/miniprojet/gestionuser/GestionUserApplication.java
+++ gestion-user/src/main/java/com/miniprojet/gestionuser/GestionUserApplication.java
@@ -2,8 +2,10 @@ package com.miniprojet.gestionuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestionUserApplication {
 
 	public static void main(String[] args) {
